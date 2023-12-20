cb8e38e2b9e096103030630f32694e6ca7b025d9
Pedro Zamorano <77652129+Pedro-Zamorano@users.noreply.github.com>
2023-12-20 00:55:13 -0300

    backend commit

---
+++ backend/src/main/java/zl/apirest/backend/repository/ScheduleRepository.java
@@ -0,0 +1,12 @@
+package zl.apirest.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import zl.apirest.backend.model.Schedule;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    public Schedule findAllById(Long id);
+
+}
